a0984a6938ecd09bf61b4920ae87546060d3c11a
fridolin <fridolin_1@yahoo.com>
2025-06-10 08:36:23 +0200

    Basic Layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,79 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="gridContainer">
+    <header>
+      <h1>Essens Kalkulator</h1>
+    </header>
+    <div id="account">
+      <RouterLink to="/login">Login</RouterLink>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <nav>
+      <p>
+        <RouterLink to="/ingredients">Zutaten</RouterLink>
+      </p>
+      <p>
+        <RouterLink to="/recipes">Rezepte</RouterLink>
+      </p>
+      <p>
+        <RouterLink to="/plans">Pläne</RouterLink>
+      </p>
+    </nav>
+    <main>
+      <h2>Wilkommen zum Essensklakulator</h2>
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1, h2 {
+  text-align: center;
 }
 
 @media (min-width: 1024px) {
+  #gridContainer {
+    height: 100%;
+    display: grid;
+
+    grid-template-rows: min-content 1fr;
+    grid-template-columns: min-content 1fr;
+    gap: 20px;
+  }
+
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    grid-row-start: 1;
+    grid-column-start: 2;
+
+    border: 1px solid var(--color-border);
+    border-top: none;
+    border-bottom-left-radius: 25px;
+    border-bottom-right-radius: 25px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  #account {
+    grid-row: 1;
+    grid-column: 1;
+    width: max-content;
+    height: max-content;
+    place-self: center;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  nav {
+    grid-row: 2;
+    grid-column: 1;
+
+    border: 1px solid var(--color-border);
+    border-left: none;
+    border-bottom: none;
+    border-top-right-radius: 25px;
+    padding: 10px 10px 0 0;
+  }
+
+  main {
+    grid-row: 2;
+    grid-column: 2;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHashHistory} from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: "/",
+            component: () => import('./views/Home.vue')
+        },
+        {
+            path: "/login",
+            component: () => import('./views/Login.vue')
+        },
+        {
+            path: "/ingredients",
+            component: () => import('./views/Ingredients.vue')
+        },
+        {
+            path: "/recipes",
+            component: () => import('./views/Recipes.vue')
+        },
+        {
+            path: "/plans",
+            component: () => import('./views/Plans.vue')
+        }
+    ]
+})
+
+createApp(App).use(router).mount('#app')
