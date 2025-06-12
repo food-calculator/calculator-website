e647841a73964905a7bff47bf2ecdda4ed5279fa
fridolin <fridolin_1@yahoo.com>
2025-06-12 08:52:27 +0200

    #2 Editor: Ingredient selection layout

--- src/Components/recipe/RecipeEditor.vue
+++ src/Components/recipe/RecipeEditor.vue
@@ -25,6 +25,12 @@ export default defineComponent({
     }
   },
   methods: {
+    addIngredient(id) {
+
+    },
+    removeIngredient(id) {
+
+    },
     save() {
 
     }
@@ -59,9 +65,27 @@ export default defineComponent({
         <p>Bilder</p>
         <p><b>TODO</b></p>
       </div>
-      <div class="inputContainer">
+      <div class="inputContainer" id="ingredientContainer">
         <p>Zutaten</p>
-        <p><b>TODO</b></p>
+        <div id="ingredientList">
+          <div v-for="ingredient in this.recipe.ingredients" class="ingredient">
+            <span>❌</span>
+            <p>{{ ingredient.name }}, {{ this.ingredients[ingredient["ingredientID"]]["unit"] }}</p>
+            <input type="number">
+          </div>
+          <div class="ingredient">
+            <label for="selectIngredient">Zutat: </label>
+            <select name="selectIngredient">
+              <optgroup label="Erstellen">
+                <option>Neue Zutat</option>
+              </optgroup>
+              <optgroup label="Zutaten">
+                <option v-for="ingredient in this.ingredients">{{ ingredient.name }}</option>
+              </optgroup>
+            </select>
+            <input type="button" value="Hinzufügen">
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -69,7 +93,7 @@ export default defineComponent({
     <p>Beschreibung</p>
     <textarea cols="40" rows="3"/>
   </div>
-  <button @click="save">Speichern</button>
+  <button id="save" @click="save">Speichern</button>
 </template>
 
 <style scoped>
@@ -82,7 +106,7 @@ h2 {
   width: max-content;
 }
 
-button {
+#save {
   position: relative;
   left: 50%;
   transform: translateX(-50%);
@@ -106,6 +130,42 @@ div:has(.inputContainer) {
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
+  max-width: 100%;
   gap: 30px;
 }
+
+#ingredientContainer {
+  flex-direction: column;
+  gap: 10px;
+  border: 1px solid var(--color-border);
+  padding: 10px;
+  border-radius: 10px;
+}
+
+#ingredientList {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  flex-wrap: wrap;
+}
+
+.ingredient {
+  border: 1px solid var(--color-border);
+  border-radius: 5px;
+  padding: 5px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 10px;
+}
+
+.ingredient span {
+  color: red;
+  font-size: 1.5em;
+  line-height: 1em;
+  cursor: pointer;
+}
+
+
 </style>
